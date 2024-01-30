abedabc1d00eeb015847e965c6257fe02d56e98f
Sumshi <sumayasomow@gmail.com>
2024-01-30 16:09:01 +0300

    included labels

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+          {/* Email input */}
+          <label htmlFor="email">Email: </label>
+          <input type="email"></input>
+
+          {/* Password input */}
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password"></input>
+
+          {/* Submit button */}
+          <button>OK</button>
+      </body>
+
+      <footer className="App-footer">
+        <p>{`${getFooterCopy(true)} - ${getFullYear()}`}</p>
+      </footer>
+      
+    </div>
+  );
+}
+
+export default App;
